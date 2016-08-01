51c22e495294378978f7850ecd1776a76e0a7df8
Cory Tohill <corytohill@gmail.com>
2016-08-01 15:47:20 -0500

    added main menu functionality and comments

--- birdyboard_menu.py
+++ birdyboard_menu.py
@@ -12,36 +12,57 @@ class BirdyBoardMenu():
         self.show_main_menu()
 
     def show_main_menu(self):
-        main_menu_options = [
-            '1. New User Account',
-            '2. Select User',
-            '3. View Chirps',
-            '4. Public Chirp',
-            '5. Private Chirp',
-        ]
+        main_menu_options = {
+            '1. New User Account': self.show_new_user_inputs,
+            '2. Select User': self.show_user_select_menu,
+            '3. View Chirps': self.show_chirps_menu,
+            '4. Public Chirp': self.show_chirp_inputs,
+            '5. Private Chirp': self.show_private_chirp_user_menu
+        }
+
         print("Please choose an option below,")
         print("or type 'exit' to leave the program:")
         print("")
 
-        for option in main_menu_options:
-            print(option)
+        # prints all keys in main_menu_options
+        for key in sorted(main_menu_options):
+            print(key)
 
         main_menu_input = input("> ")
 
+        # exits the program if user enters exit
         if main_menu_input.lower() == "exit":
             quit()
 
+        # if the user input is in one of the keys in the main_menu_dict
+        # the main_menu_input is redifined to be that key
+        for key in main_menu_options:
+            if main_menu_input in key:
+                main_menu_input = key
+
+        # displays the option the user selected
+        print("You chose {}".format(main_menu_input[3:]))
+        print("")
+
+        # calls the appropriate function in main_menu_options based on user's input
+        main_menu_options[main_menu_input]()
+
     def show_new_user_inputs(self):
-        pass
+        print("new user inputs")
+        self.show_main_menu()
 
     def show_user_select_menu(self):
-        pass
+        print("user select menu")
+        self.show_main_menu()
 
     def show_chirps_menu(self):
-        pass
+        print("show chirps menu")
+        self.show_main_menu()
 
     def show_chirp_inputs(self):
-        pass
+        print("show chirps inputs")
+        self.show_main_menu()
 
     def show_private_chirp_user_menu(self):
-        pass
+        print("show private chirp user menu")
+        self.show_main_menu()

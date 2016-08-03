5bc35e6cff40743881fc935387713af649f53a31
Cory Tohill <corytohill@gmail.com>
2016-08-03 13:32:45 -0500

    added initial methods to birdyboard and fixed errors in tests

--- Test_Birdyboard.py
+++ Test_Birdyboard.py
@@ -2,27 +2,28 @@ import unittest
 from birdyboard import *
 
 
-
 class TestBirdyBoard(unittest.TestCase):
 
     @classmethod
     def setUpClass(self):
         self.birdyboard = BirdyBoard()
-        self.user1 = birdyboard.new_user("Full name", "Screen Name")
-        self.user2 = birdyboard.new_user("Other full name", "Other screen name")
+        self.birdyboard.create_user("Full name", "Screen Name")
+        self.birdyboard.create_user("Other full name", "Other screen name")
 
-    def test_create_new_user_appends_to_users_list(self):
-        self.assertEqual(self.birdyboard.users[0], self.user1)
-        self.assertEqual(self.birdyboard.users[1], self.user2)
+    def test_create_user_appends_to_users_list(self):
+        self.assertEqual(self.birdyboard.users[0].full_name, "Full name")
+        self.assertEqual(self.birdyboard.users[1].screen_name, "Other screen name")
 
     def test_create_new_chirp_appends_to_chirps_list(self):
-        chirp1 = self.birdyboard.new_chirp("Hello", self.user1.user_id)
+        self.birdyboard.create_chirp("Hello", "abc123")
 
-        self.assertEqual(self.birdyboard.chirps[0], chirp1)
+        self.assertEqual(self.birdyboard.chirps[0].message, "Hello")
+        self.assertEqual(self.birdyboard.chirps[0].author, "abc123")
 
     def test_set_user_sets_current_user(self):
-        self.birdyboard.set_current_user(self.user1)
-        self.assertEqual(self.birdyboard.current_user, self.user1)
+        test_user = User("Test McTest", "I'm so testy")
+        self.birdyboard.set_current_user(test_user)
+        self.assertEqual(self.birdyboard.current_user, test_user)
 
     def test_conversation_dict_exists(self):
         self.assertIsNotNone(self.birdyboard.conversations)
--- birdyboard.py
+++ birdyboard.py
@@ -1,3 +1,4 @@
+import uuid
 from user import *
 from chirp import *
 
@@ -5,42 +6,23 @@ from chirp import *
 class BirdyBoard():
 
     def __init__(self):
-        pass
-
-    def new_user(self, screen_name, full_name):
-        user = User()
-        self.users.append(user)
-        self.set_current_user(user)
+        self.chirps = []
+        self.users = []
+        self.current_user = None
+        self.conversations = {}
 
     def set_current_user(self, user_id):
         self.current_user = user_id
 
-# import uuid
+    def create_user(self, full_name, screen_name):
+        user = User(full_name, screen_name)
+        self.users.append(user)
+        self.set_current_user(user)
 
-
-# class BirdyBoard():
-
-#     def __init__(self):
-#         self.chirps = []
-#         self.users = []
-#         self.current_user = None
-#         self.replies = []
-
-
-
-#     def create_chirp(self, message, is_private, chirped_at_user_id):
-#         chirp = {
-#             'message': message,
-#             'is_private': is_private,
-#             'chirped_at_user_id': chirped_at_user_id,
-#             'chirp_id': uuid.uuid4(),
-#             'author': self.current_user['user_id']
-#         }
-#         self.chirps.append(chirp)
-
-#     def reply_to_chirp(self, message, chirp_id):
-#         reply = {
-#             'message': message,
-#             'chirp_id': chirp_id
-#         }
-#         self.replies.append(reply)
+    def create_chirp(self, message, user_id, private=False, receiver_id=None, conversation_id=None):
+        new_chirp = Chirp(message, user_id, private, receiver_id)
+        self.chirps.append(new_chirp)
+        if conversation_id is None:
+            self.conversations[uuid.uuid4()] = [new_chirp.chirp_id]
+        else:
+            conversations[conversation_id].append(new_chirp.chrip_id)
